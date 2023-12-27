f9020cca4722a545da466e77fe8d826fdf59095b
allantabilog <allan.tabilog@gmail.com>
2023-12-28 10:45:52 +1100

    Add expectations to unit tests

--- packages/runtime/src/__tests__/h.test.js
+++ packages/runtime/src/__tests__/h.test.js
@@ -11,17 +11,57 @@ import { h, hFragment, hString } from "../h";
 import { test, expect } from "vitest";
 
 test("generates a virtual dom correctly", () => {
-  console.log(hString("hello"));
-  console.log(h("h1", { class: "title" }, [hString("My counter")]));
+  let h1_1 = hString("hello");
+  expect(h1_1).toMatchObject({ type: "text", value: "hello" });
+
+  let h1_2 = h("h1", { class: "title" }, [hString("My counter")]);
+  expect(h1_2).toMatchObject({
+    tag: "h1",
+    props: { class: "title" },
+    children: [{ type: "text", value: "My counter" }],
+    type: "element",
+  });
 
   let h1_title = h("h1", { class: "title" }, [hString("My counter")]);
   let h2_name = h("h2", { class: "name" }, [hString("My counter")]);
 
   let fragments = hFragment([h2_name, h1_title]);
-  console.log(fragments);
+  expect(fragments).toMatchObject({
+    type: "fragment",
+    children: [
+      {
+        tag: "h2",
+        props: { class: "name" },
+        children: [{ type: "text", value: "My counter" }],
+        type: "element",
+      },
+      {
+        tag: "h1",
+        props: { class: "title" },
+        children: [{ type: "text", value: "My counter" }],
+        type: "element",
+      },
+    ],
+  });
 
   let fragmentsWithNull = hFragment([h2_name, null, h1_title]);
-  console.log(fragmentsWithNull);
+  expect(fragmentsWithNull).toMatchObject({
+    type: "fragment",
+    children: [
+      {
+        tag: "h2",
+        props: { class: "name" },
+        children: [{ type: "text", value: "My counter" }],
+        type: "element",
+      },
+      {
+        tag: "h1",
+        props: { class: "title" },
+        children: [{ type: "text", value: "My counter" }],
+        type: "element",
+      },
+    ],
+  });
 });
 
 test("generates a virtual dom correctly = part 2", () => {
@@ -34,5 +74,50 @@ test("generates a virtual dom correctly = part 2", () => {
     ]),
   ]);
 
-  console.log(JSON.stringify(fragment, null, 2));
+  expect(fragment).toMatchObject({
+    type: "fragment",
+    children: [
+      {
+        tag: "h1",
+        props: { class: "title" },
+        children: [{ type: "text", value: "My counter" }],
+        type: "element",
+      },
+      {
+        tag: "div",
+        props: { class: "container" },
+        children: [
+          {
+            tag: "button",
+            props: {},
+            children: [{ type: "text", value: "decrement" }],
+            type: "element",
+          },
+          {
+            tag: "span",
+            props: {},
+            children: [{ type: "text", value: "0" }],
+            type: "element",
+          },
+          {
+            tag: "button",
+            props: {},
+            children: [{ type: "text", value: "increment" }],
+            type: "element",
+          },
+        ],
+        type: "element",
+      },
+    ],
+  });
+});
+
+test("generate text nodes", () => {
+  let div = h("div", { id: "test" }, ["hello"]);
+  expect(div).toMatchObject({
+    tag: "div",
+    props: { id: "test" },
+    children: [{ type: "text", value: "hello" }],
+    type: "element",
+  });
 });
